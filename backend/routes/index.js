6313ab8e5c1ad267954843fde94db1ebfdb03217
Kushal1306 <kalakushal.jain@gmail.com>
2024-08-22 13:09:49 +0530

    adding payment webhook @lemon sequeezy

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,6 +4,7 @@ import questionRouter from "./questionRoute.js";
 import quizRouter from "./quizRoute.js";
 import scoreRouter from "./scoreRoute.js"
 import testRouter from './ragRoute.js';
+import subscriptionRouter from './subscriptionRoute.js';
 
 
 const mainRouter=express.Router();
@@ -12,6 +13,7 @@ mainRouter.use("/user",UserRouter);
 mainRouter.use("/quiz",quizRouter);
 mainRouter.use("/question",questionRouter);
 mainRouter.use("/score",scoreRouter);
+mainRouter.use("/subscription",subscriptionRouter);
 // mainRouter.use("/model",testRouter);
 
 
---
+++ backend/routes/subscriptionRoute.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import authMiddleware from '../middlewares/authMiddleware.js';
+
+const subscriptionRouter=express.Router();
+
+subscriptionRouter.post("/success",(req,res)=>{
+  const event=req.body;
+  console.log(req.body);
+  if(event&&event.event==='subscription.success'){
+    const subscriptionData=event.data;
+    console.log(subscriptionData);
+    res.status(200).send('Webhook received');
+  }
+  else{
+    res.status(400).send('Invalid event type');
+  }
+
+});
+
+subscriptionRouter.post("/failed",(req,res)=>{
+    const event=req.body;
+    console.log(req.body);
+    if(event&&event.event==='subscription.failed'){
+      const subscriptionData=event.data;
+      console.log(subscriptionData);
+      res.status(200).send('Webhook received');
+    }
+    else{
+      res.status(400).send('Invalid event type');
+    }
+  
+  });
+export default subscriptionRouter;
